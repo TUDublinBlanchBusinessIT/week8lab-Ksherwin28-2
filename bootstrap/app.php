4ee97d2cce0356cb6daad92eab1ecf48731495e2
shero121 <keithsherwin6764@gmail.com>
2024-03-11 15:16:01 +0000

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -71,6 +71,10 @@ $app->configure('app');
 | route or middleware that'll be assigned to some specific routes.
 |
 */
+
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
