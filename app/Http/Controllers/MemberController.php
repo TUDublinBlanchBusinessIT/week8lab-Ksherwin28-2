be44f1fe636740ddcd58455e73f96e473f8910c7
shero121 <keithsherwin6764@gmail.com>
2024-03-11 15:16:15 +0000

    pretty printing working

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Member as Member;
+use Illuminate\Http\Request;
+class MemberController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+    public function showAllMembers()
+    {
+        $members = Member::all();
+        return response()->json($members);
+    }
+    public function showOneMember($id)
+    {
+        return response()->json(Member::find($id));
+    }
+
+
+
+    public function showMemberBookings($id)
+    {
+    $bookings = Booking::all()->where('memberid',$id);
+    return response()->json($bookings);
+    }
+}
---
+++ app/Models/Member.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Member extends Model
+{
+    public function __construct($type = null)
+    {
+        parent::__construct();
+        $this->setTable("Member");
+    }
+    public $timestamps=false;
+    protected $fillable = [
+        'firstname', 'surname', 'membertype', 'dateofbirth'
+    ];
+    protected $hidden = [
+       'userid','created_at', 'updated_at', 'deleted_at'
+    ];
+
+
+
+
+    protected $appends = ['_links'];
+    public function getLinksAttribute()
+    {
+        return [
+            ['self' => app()->make('url')->to("api/members/{$this->attributes['id']}")],
+            ['bookings' => app()->make('url')->to("api/member/{$this->attributes['id']}/bookings")]
+        ];
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(\App\Models\Booking::class, 'memberid');
+    }
+}
+
